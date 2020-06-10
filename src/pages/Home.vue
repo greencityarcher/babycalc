d6551c27d07b3f01efc9675414533345ebd0a3d6
greencityarcher <pingttl128@gmail.com>
2020-06-10 13:47:18 +0300

    multipage structure added

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <template>
+    <main-layout>
+      <p>Welcome home</p>
+    </main-layout>
+  </template>
+
+  <script>
+    import MainLayout from '../layouts/Main.vue'
+    export default {
+      components: {
+        MainLayout
+      }
+    }
+  </script>
