b64eacb251a99f6022a364d83fa6bdd0fb802613
Victoria <pingttl128@gmail.com>
2021-05-05 17:21:28 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-//  publicPath: process.env.NODE_ENV === 'production'
-    //? '/babycalc/'
-  //  ? '/babycalc/'
-  //  : '/'
-     baseUrl: ‘/babycalc/’
+  publicPath: process.env.NODE_ENV === 'production'
+   ? '/babycalc/'
+    ? '/babycalc/'
+    : '/'
+    
 }
